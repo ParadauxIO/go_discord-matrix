ddcbdcfa633dce9ea93700dac198ee85872a281f
rian <rian@paradaux.io>
2021-10-17 12:22:48 +0100

    WIP webhook api

--- discord_webhooks.go
+++ discord_webhooks.go
@@ -1,11 +1,19 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 type WebhookMessage struct {
-	Content   string
-	Username  string
-	AvatarUrl string
-	TTS       bool
-	Embeds    []Embed
+	content    string
+	username   string
+	avatar_url string
+	tts        bool
+	embeds     []Embed
 }
 
 type Embed struct {
@@ -17,6 +25,36 @@ type MessageResult struct {
 }
 
 func sendMessage(webhook string, message WebhookMessage) MessageResult {
+	byteValue, err := json.Marshal(&message)
+
+	if err != nil {
+		return MessageResult{} // TODO err obj
+	}
+
+	var jsonString string
+	json.Unmarshal(byteValue, &jsonString)
+	fmt.Printf("%#v", jsonString)
+
+	httpReq, httpErr := http.NewRequest("POST", webhook, bytes.NewBuffer(byteValue))
+	httpReq.Header.Set("Content-Type", "application/json")
+
+	if httpErr != nil {
+		return MessageResult{} // TODO err obj
+	}
+
+	httpClient := &http.Client{}
+	httpResp, httpErr := httpClient.Do(httpReq)
+
+	if httpErr != nil {
+		return MessageResult{} // TODO err obj
+	}
+
+	defer httpResp.Body.Close()
+
+	fmt.Println("response Status:", httpResp.Status)
+	fmt.Println("response Headers:", httpResp.Header)
+	body, _ := ioutil.ReadAll(httpResp.Body)
+	fmt.Println("response Body:", string(body))
 
 	return MessageResult{}
 
