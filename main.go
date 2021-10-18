fe95884c0923e52986c97ccc6f11a0839ab039f7
Rían Errity <rian@paradaux.io>
2021-10-18 16:21:58 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,5 +6,13 @@ func main() {
 	fmt.Println("Matrix <-> Discord Bridge")
 
 	fmt.Println("... loading configuration")
-	loadConfig()
+	//loadConfig()
+
+	sendMessage(webhook, WebhookMessage{
+		Content:   "This is a test",
+		Username:  "Bridge",
+		AvatarUrl: "https://pbs.twimg.com/profile_images/510528591424024576/Fzk0vaGr.png",
+		Tts:       false,
+		Embeds:    []Embed{},
+	})
 }
